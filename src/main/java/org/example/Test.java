1707d4874da62e888bd1f606b9a38c41491ca271
Касьянов Михаил Юрьевич <vtb4126802@corp.dev.vtb>
2026-07-01 16:15:03 +0500

    2

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,5 +1,25 @@
 package org.example;
 
 public class Test {
-
+    public void test() {
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+    }
 }
+
