ffba147e248cfd7bc8ac4ae83438b173dc532c6e
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2026-07-01 15:42:37 +0500

    Feature/new (#4)
    * Test1
    
    * Test2
    
    * Test3
    
    * Test4
    
    ---------
    
    Co-authored-by: Касьянов Михаил Юрьевич <vtb4126802@corp.dev.vtb>

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,5 +1,10 @@
 package org.example;
 
 public class Test {
+    private  Test testik=new Test();
+    private Test test2= new Test();
 
+    public Test getTest2() {
+        return test2;
+    }
 }
