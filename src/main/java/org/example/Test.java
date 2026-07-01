0a671ae5168d4e2ca7d364ffb6a528b819fe1263
Касьянов Михаил Юрьевич <vtb4126802@corp.dev.vtb>
2026-07-01 16:00:57 +0500

    Testt_1

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,5 +1,34 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Test {
-    private  Test testik=new Test();
+    private String mew = new String();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Test test = (Test) o;
+        return Objects.equals(mew, test.mew);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(mew);
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "mew='" + mew + '\'' +
+                '}';
+    }
+
+    public String getMew() {
+        return mew;
+    }
+
+    public void setMew(String mew) {
+        this.mew = mew;
+    }
 }
