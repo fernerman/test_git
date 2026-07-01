e2c963dac1499a27d29c4eba0864f8b65ffbb35c
Касьянов Михаил Юрьевич <vtb4126802@corp.dev.vtb>
2026-07-01 16:01:04 +0500

    Testt_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-
+        System.out.println("mew");
     }
 }
