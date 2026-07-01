3f509a97a387d4f8bf8c58134ebd4cd33d18e02d
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2026-07-01 15:49:50 +0500

    Feature/new (#5)
    * Test1
    
    * Test2
    
    * Test3
    
    * Test4
    
    * Test_1
    
    * Test_2
    
    ---------

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("oooooooo");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+
     }
 }
