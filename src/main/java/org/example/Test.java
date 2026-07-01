f92bf60ba09f768e3ae44234559c3443ea297ece
Касьянов Михаил Юрьевич <vtb4126802@corp.dev.vtb>
2026-07-01 16:01:50 +0500

    update

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,10 +1,5 @@
 package org.example;
 
 public class Test {
-    private  Test testik=new Test();
-    private Test test2= new Test();
 
-    public Test getTest2() {
-        return test2;
-    }
 }
